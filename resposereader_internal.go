7bcbbbc0579eb139e68ef8800f3005150f2332de
Charles Iliya Krempeaux <iamreiver@gmail.com>
2022-11-24 13:02:08 -0800

    initial commits

---
+++ resposereader_internal.go
@@ -0,0 +1,99 @@
+package finger
+
+import (
+	"github.com/reiver/go-utf8"
+
+	"fmt"
+	"io"
+	"net"
+)
+
+var _ ResponseReader = internalResponseReader{}
+
+type internalResponseReader struct {
+	conn net.Conn
+}
+
+func (receiver internalResponseReader) Close() error {
+
+	var conn net.Conn
+	{
+		conn = receiver.conn
+		if nil == conn {
+			return errNilConnection
+		}
+	}
+
+	return conn.Close()
+}
+
+func (receiver internalResponseReader) LocalAddr() net.Addr {
+
+	var conn net.Conn
+	{
+		conn = receiver.conn
+		if nil == conn {
+			return nil
+		}
+	}
+
+	return conn.LocalAddr()
+}
+
+func (receiver internalResponseReader) Read(p []byte) (int, error) {
+
+	var conn net.Conn
+	{
+		conn = receiver.conn
+		if nil == conn {
+			return 0, errNilConnection
+		}
+	}
+
+	return conn.Read(p)
+}
+
+func (receiver internalResponseReader) ReadByte() (byte, error) {
+
+	var buffer [1]byte
+	var p []byte = buffer[:]
+
+	n, err := receiver.Read(p)
+	if nil != err {
+		return 0, fmt.Errorf("problem reading byte from finger-protocol response: %w", err)
+	}
+	if expected, actual := 1, n; expected != actual {
+		return 0, fmt.Errorf("problem reading byte from finger-protocol response: actual number of bytes read is %d but expected it to be %d", actual, expected)
+	}
+
+	return buffer[0], nil
+}
+
+func (receiver internalResponseReader) ReadRune() (rune, int, error) {
+
+	var conn net.Conn
+	{
+		conn = receiver.conn
+		if nil == conn {
+			return utf8.RuneError, 0, errNilConnection
+		}
+	}
+
+	var wrapped utf8.RuneReader = utf8.RuneReaderWrap(conn)
+	var runereader io.RuneReader = &wrapped
+
+	return runereader.ReadRune()
+}
+
+func (receiver internalResponseReader) RemoteAddr() net.Addr {
+
+	var conn net.Conn
+	{
+		conn = receiver.conn
+		if nil == conn {
+			return nil
+		}
+	}
+
+	return conn.RemoteAddr()
+}
