7d8c0b1dc049d73dddb937ddeb66af5ba4dc2719
Charles Iliya Krempeaux <iamreiver@gmail.com>
2022-11-24 02:40:43 -0800

    initial commits

---
+++ client.go
@@ -0,0 +1,71 @@
+package finger
+
+import (
+	"net"
+)
+
+// A Client is a finger-protocol client.
+//
+// It handles sending a finger-protocol client request and receiving a finger-protocol server response.
+type Client struct {
+	conn net.Conn
+}
+
+// AssebleClient returns a client that communicates over a net.Conn.
+//
+// Example
+//
+//	var conn net.Conn
+//	
+//	// ...
+//	
+//	var fingerClient finger.Client = finger.AssembleClient(conn)
+func AssembleClient(conn net.Conn) Client {
+	return Client{
+		conn:conn,
+	}
+}
+
+// Close closes the wrapped net.Conn.
+func (receiver *Client) Close() error {
+	if nil == receiver {
+		return nil
+	}
+
+	var conn net.Conn
+	{
+		conn = receiver.conn
+		if nil == conn {
+			return nil
+		}
+	}
+
+	return conn.Close()
+}
+
+// LocalAddr returns the local-address of the connection, if known.
+func (receiver Client) LocalAddr() net.Addr {
+	var conn net.Conn
+	{
+		conn = receiver.conn
+		if nil == conn {
+			return nil
+		}
+	}
+
+	return conn.LocalAddr()
+}
+
+// RemoteAddr returns the remote-address of the connection, if known.
+func (receiver Client) RemoteAddr() net.Addr {
+	var conn net.Conn
+	{
+		conn = receiver.conn
+		if nil == conn {
+			return nil
+		}
+	}
+
+	return conn.RemoteAddr()
+}
+
