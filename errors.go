9199b21e0d59bdf34da06e4634c1d786d0749984
Charles Iliya Krempeaux <iamreiver@gmail.com>
2022-12-14 10:53:16 -0800

    initial commits

--- errors.go
+++ errors.go
@@ -5,6 +5,8 @@ import (
 )
 
 const (
+	errNilConnection  = fck.Error("nil connection")
+	errNilHandler     = fck.Error("nil handler")
 	errNilListener    = fck.Error("nil listener")
 	errNilReceiver    = fck.Error("nil receiver")
 	errNilWriter      = fck.Error("nil writer")
