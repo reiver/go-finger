bb9e8194267d0ab8276958c3eb8d8807565b0b84
Charles Iliya Krempeaux <iamreiver@gmail.com>
2022-12-14 10:48:02 -0800

    initial commits

---
+++ test/errors.go
@@ -0,0 +1,10 @@
+package testfinger
+
+import (
+	"github.com/reiver/go-fck"
+)
+
+const (
+	errClosed      = fck.Error("closed")
+	errNilReceiver = fck.Error("nil receiver")
+)
---
+++ test/testconnwriter.go
@@ -0,0 +1,104 @@
+package testfinger
+
+import (
+	"github.com/reiver/go-arbitrary"
+
+	"net"
+	"time"
+)
+
+// TestConnectedWriteCloser is a finger.ConnectedWritCloser that can be used for tsting purposes.
+//
+// What has been written into it can be retreived with its String method
+//
+// Example usage:
+//
+//	var buffer testfinger.TestConnectedWriteCloser
+//	
+//	var responsewriter finger.ResponseWriter = finger.NewResponseWriter(&buffer)
+//	
+//	// ...
+//
+//	var writtenData string = buffer.String()
+type TestConnectedWriteCloser struct {
+	storage []byte
+	closed bool
+	localAddr net.Addr
+	remoteAddr net.Addr
+}
+
+func (receiver *TestConnectedWriteCloser) Close() error {
+	if nil == receiver {
+		return errNilReceiver
+	}
+
+	receiver.closed = true
+
+	return nil
+}
+
+func (receiver *TestConnectedWriteCloser) LocalAddr() net.Addr {
+	if nil == receiver {
+		return nil
+	}
+
+	if receiver.closed {
+		return nil
+	}
+
+	if nil == receiver.localAddr {
+		receiver.localAddr = arbitrary.NetAddr()
+	}
+
+	return receiver.localAddr
+}
+
+func (receiver *TestConnectedWriteCloser) RemoteAddr() net.Addr {
+	if nil == receiver {
+		return nil
+	}
+
+	if receiver.closed {
+		return nil
+	}
+
+	if nil == receiver.remoteAddr {
+		receiver.remoteAddr = arbitrary.NetAddr()
+	}
+
+	return receiver.remoteAddr
+}
+
+func (receiver *TestConnectedWriteCloser) SetWriteDeadline(t time.Time) error {
+	if nil == receiver {
+		return errNilReceiver
+	}
+
+//@TODO
+	return nil
+}
+
+func (receiver *TestConnectedWriteCloser) String() string {
+	if nil == receiver {
+		return ""
+	}
+
+	return string(receiver.storage)
+}
+
+func (receiver *TestConnectedWriteCloser) Write(p []byte) (int, error) {
+	if nil == receiver {
+		return 0, errNilReceiver
+	}
+
+	if receiver.closed {
+		return 0, errClosed
+	}
+
+	if len(p) <= 0 {
+		return 0, nil
+	}
+
+	receiver.storage = append(receiver.storage, p...)
+	return len(p), nil
+}
