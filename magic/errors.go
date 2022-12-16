de7714a506eb1e1edb2e487c86a3ccf6e8511d8a
Charles Iliya Krempeaux <iamreiver@gmail.com>
2022-12-16 12:53:59 -0800

    initial commits

--- magic/errors.go
+++ magic/errors.go
@@ -5,11 +5,11 @@ import (
 )
 
 const (
-	errBadFieldName           = fck.Error("bad field name")
-	errBadFieldBody           = fck.Error("bad field body")
-	errNilFile                = fck.Error("nil file")
-	errNilMagicResponseWriter = fck.Error("nil magic response writer")
-	errNilReceiver            = fck.Error("nil receiver")
-	errNilResponseWriter      = fck.Error("nil response writer")
-	errNilWriter              = fck.Error("nil writer")
+	errBadFieldName      = fck.Error("bad field name")
+	errBadFieldBody      = fck.Error("bad field body")
+	errNilFile           = fck.Error("nil file")
+	errInternalError     = fck.Error("internal error")
+	errNilReceiver       = fck.Error("nil receiver")
+	errNilResponseWriter = fck.Error("nil response writer")
+	errNilWriter         = fck.Error("nil writer")
 )
