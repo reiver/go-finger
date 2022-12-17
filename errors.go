5dfbdfaea95a5f80de0889a8b3ab44ec5cba91dc
Charles Iliya Krempeaux <iamreiver@gmail.com>
2022-12-16 23:14:28 -0800

    initial commits

--- errors.go
+++ errors.go
@@ -5,6 +5,13 @@ import (
 )
 
 const (
+	ErrClientErred   = fck.Error("client erred")
+	ErrServerErred   = fck.Error("server erred")
+	ErrServerFailed  = fck.Error("server failed")
+	ErrServerRefused = fck.Error("server refused")
+)
+
+const (
 	errInternalError  = fck.Error("internal error")
 	errNilConnection  = fck.Error("nil connection")
 	errNilHandler     = fck.Error("nil handler")
